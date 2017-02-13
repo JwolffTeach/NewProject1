2c530ab13b436db99b79d35d1b96a0d4b5113103
JwolffTeach <jwolff@dsdmail.net>
2017-02-13 14:47:06 -0700

    Slerp + Particle Trail

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+    
+    public Transform to;
+    public float speed = 10f;
+    public float gameTime=0f;
+    public float startTime;
+    public bool ismoving = false;
+
+    // Use this for initialization
+    void Start () {
+        startTime = Time.time;
+        gameTime = startTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position = Vector3.Slerp(transform.position, to.position, speed);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, to.rotation, speed);
+	}
+
+    void FixedUpdate() {
+        gameTime = Time.time;
+        //Debug.Log(gameTime);
+        if (gameTime-startTime >= 3 && ismoving == false) {
+            StartCoroutine(moveGoal(to));
+            ismoving = true;
+        }
+    }
+
+    IEnumerator moveGoal (Transform target) {
+        while (Vector3.Distance(transform.position, target.position) > 0.05f) {
+            transform.position = Vector3.Lerp(transform.position, target.position, speed);
+
+            yield return null;
+        }
+
+        print("Reached the target.");
+
+        yield return new WaitForSeconds(3f);
+
+        print("MyCoroutine is now finished.");
+        ismoving = false;
+        startTime = Time.time;
+    }
+}
---
+++ Assets/FollowMouse.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FollowMouse : MonoBehaviour {
+
+    private Vector3 _target;
+    public Camera Camera;
+    public bool FollowtheMouse;
+    public bool ParticlesAccelerates;
+    public float ParticlesSpeed = 2.0f;
+
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    public void OnEnable() {
+        if (Camera == null) {
+            throw new InvalidOperationException("Camera not set");
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if (FollowtheMouse || Input.GetMouseButton(0)) {
+            _target = Camera.ScreenToWorldPoint(Input.mousePosition);
+            _target.z = 0;
+        }
+
+        var delta = ParticlesSpeed * Time.deltaTime;
+        if (ParticlesAccelerates) {
+            delta *= Vector3.Distance(transform.position, _target);
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _target, delta);
+	}
+}
